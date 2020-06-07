1bd1fef78ad1d05e05aa8f203e96894ef8b992cb
Amit Kumar <offamitkumar@gmail.com>
2020-06-07 13:20:26 +0530

    rotate the sentence , output formatting

---
+++ Solution/Problem_000490/490 - Rotating Sentences.cpp
@@ -0,0 +1,76 @@
+//  Author : Amit Kumar 
+
+//  #include <ext/pb_ds/assoc_container.hpp>
+//  #include <ext/pb_ds/tree_policy.hpp>
+
+  #include <iostream>
+  #include <algorithm>
+  #include <string>
+//  #include <sstream>
+//  #include <fstream>
+//  #include <iomanip>
+//  #include <chrono>
+//  #include <numeric>
+//  #include <utility>
+
+//  #include <bitset>
+//  #include <tuple>
+//  #include <queue>
+//  #include <stack>
+  #include <vector>
+//  #include <array>
+//  #include <unordered_map>
+//  #include <unordered_set>
+//  #include <set>
+//  #include <map>
+//  #include <deque>
+
+//  #include <climits>
+//  #include <cstring>
+//  #include <cmath>
+//  #include <cassert>
+#include <cstdio>
+using namespace std;
+//  using namespace std::chrono;
+//  using namespace placeholders;
+//  using namespace __gnu_pbds;
+//  template<typename TypeInfo>
+//  using new_set = tree< // find_by_order & order_of_key
+//          TypeInfo ,
+//          null_type ,
+//          less<TypeInfo> ,
+//          rb_tree_tag ,
+//          tree_order_statistics_node_update 
+//  > ;
+const int MAXM = (int)1e5+100;
+const int MAXN = (int)1e5+100;
+const int MOD = (int)1e5+100;
+
+int main(void){
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+    freopen("error","w",stderr);
+#endif 
+//    auto initial_time = high_resolution_clock::now();
+    vector<string>v;
+    string s; 
+    int max_length=-1;
+    while(getline(cin , s)){
+        max_length = max(max_length , (int)s.size());
+        v.push_back(s);
+    }
+    for(int i=0;i<max_length; ++i){
+        for(int j=v.size()-1;j>=0;--j){
+            if(v[j].size() > i){
+                cout << v[j][i];
+            }else{
+                cout <<' ';
+            }
+        }
+        cout << '\n'; 
+    }
+//    auto final_time = high_resolution_clock::now();
+//    cout << duration_cast<milliseconds>(final_time-initial_time).count() << " ms" << '\n'; 
+    return 0;
+}
