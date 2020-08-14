d38ffbd91928677a883c0d6b7ce4c7134c52f763
Amit Kumar <offamitkumar@gmail.com>
2020-08-14 13:37:22 +0530

    simple observation

---
+++ Solution/Problem_010970/10970 Big Chocolate.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+
+signed main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); 
+    cout.tie(nullptr);
+#ifdef HELL_JUDGE
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+#endif
+    int n, m; 
+    while(cin >> n >> m){
+        cout << n*m -1ll << '\n'; 
+    }
+    return 0;
+}
