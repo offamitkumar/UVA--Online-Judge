a5d41c462d39c312a34f3bc01ed78e45bb90ea6c
Amit Kumar <offamitkumar@gmail.com>
2020-08-24 13:12:26 +0530

    find the word

---
+++ Solution/Problem_012478/12478 Hardest Problem Ever (Easy).cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+
+signed main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); 
+    cout.tie(nullptr);
+#ifdef hell_judge
+    freopen("input","r",stdin);
+    freopen("output","w",stdout);
+#endif
+    cout <<"KABIR"<<'\n'; 
+    return 0;
+}
