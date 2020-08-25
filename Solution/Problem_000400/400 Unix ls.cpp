99d88523fd5fefd9b1cb3e7d63cddf6acea011de
Amit Kumar <offamitkumar@gmail.com>
2020-08-25 15:03:58 +0530

    break the problem into parts

--- Solution/Problem_000400/400 Unix ls.cpp
+++ Solution/Problem_000400/400 Unix ls.cpp
@@ -13,36 +13,26 @@ signed main(void){
     int n; 
     while(cin >> n){
         vector<string>v(n); 
-        int x = 0;
+        int max_len = 0;
         for(int i=0; i<n;++i){
             cin >> v.at(i);
-            x = max(x , (int)v.at(i).size());
+            max_len = max(max_len , (int)v.at(i).size());
         }
         sort(v.begin(), v.end()); 
         cout<< string(60, '-') << '\n'; 
-        int wid=x;
+        int wid=max_len;
         int counter = 1;
-        while(wid+x+2<=60){
+        while(wid+max_len+2<=60){
             ++counter;
-            wid+=(x+2);
+            wid+=(max_len+2);
         }
-        int k = ceil((double)n/(double)counter);
-        for(int i=0; i<k; ++i){
-            int ttt = counter;
-            for(int j=i;j<n; j+=k){
-                if(ttt==0){
-                    int t = v.at(j).size(); 
-                    cout << v.at(j); 
-                    for(int l = t;l<x; ++l){
-                        cout<<" ";
-                    }
-                }else{
-                    int t = v.at(j).size(); 
-                    cout << v.at(j); 
-                    for(int l = t;l<x+2; ++l){
-                        cout<<" ";
-                    }
-
+        int row = ceil((double)n/(double)counter);
+        for(int i=0; i<row; ++i){
+            for(int j=i;j<n; j+=row){
+                int t = v.at(j).size(); 
+                cout << v.at(j); 
+                for(int l = t;l<max_len+2; ++l){
+                    cout<<" ";
                 }
             }
             cout << '\n'; 
